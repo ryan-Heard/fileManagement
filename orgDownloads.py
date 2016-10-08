e791f7a800b95b353336ee3934748118b41b9c4b
Ryan Heard <brotherakuma@gmail.com>
2016-10-07 19:17:53 -1000

    Fixed matching file names bug

--- orgDownloads.py
+++ orgDownloads.py
@@ -7,6 +7,44 @@ def get_downloads():
     os.chdir(path.expanduser('~')+"\Downloads")
     results = os.listdir()
 
+    for line in results:
+        ext = line.split('.')
+
+        try:
+            switch_board(ext, line)
+        except Exception as ex:
+            print(ex)
+            ext[0] += ' (COPY)'
+            switch_board(ext, line)
+
+
+def move_files(item, target):
+    target = target.strip()
+    os.chdir(path.expanduser('~'))
+
+    if os.path.exists(target):
+        if not os.path.exists('Downloads/Copies/'):
+            os.mkdir('Downloads/Copies')
+
+        if not os.path.exists('Downloads/Copies/'+target):
+                os.mkdir('Downloads/Copies/'+target)
+
+        target = 'Downloads/Copies/'+target
+
+    elif os.path.exists('Desktop/'+target):
+        target = 'Desktop/'+target
+
+    elif not os.path.exists(target):
+        os.chdir('Documents')
+
+    shutil.move(item, path.expanduser('~')+'\\'+target)
+    print("Moved: {} to {}".format(item, target))
+
+
+def switch_board(ext, line):
+    os.chdir(path.expanduser('~')+"\Downloads")
+    location = path.realpath(line)
+
     img_types = ['jpeg', 'gif', 'img', 'jpg', ' png']
     readable_types = ['pdf', 'doc', 'rtf',
                       'docx', 'xls', 'csv',
@@ -15,40 +53,19 @@ def get_downloads():
     sound_types = ['mp4']
     installer_types = ['exe']
 
-    for line in results:
-        os.chdir(path.expanduser('~')+"\Downloads")
-        location = path.realpath(line)
-        ext = line.split('.')
+    if len(ext) > 1:
+        ext = ext[1]
 
-        if len(ext) > 1:
-            ext = ext[1]
-
-        if ext in installer_types:
-            move_files(location, 'Desktop/Installers')
-        elif ext in readable_types:
-            move_files(location, 'Documents')
-        elif ext in img_types:
-            move_files(location, 'Pictures')
-        elif ext in sound_types:
-            move_files(location, 'Videos')
-        elif ext in compressed_file_types:
-            move_files(location, 'Setups')
-
-
-def move_files(item, target):
-    target = target.strip()
-    os.chdir(path.expanduser('~'))
-
-    if os.path.exists(target):
-        pass
-    elif os.path.exists('Desktop/'+target):
-        target = 'Desktop/'+target
-    elif not os.path.exists(target):
-        os.chdir('Desktop')
-        os.mkdir(target)
-
-    shutil.move(item, path.expanduser('~')+'\\'+target)
-    print("Moved: {} to {}".format(item, target))
+    if ext in installer_types:
+        move_files(location, 'Installers')
+    elif ext in readable_types:
+        move_files(location, 'Documents')
+    elif ext in img_types:
+        move_files(location, 'Pictures')
+    elif ext in sound_types:
+        move_files(location, 'Videos')
+    elif ext in compressed_file_types:
+        move_files(location, 'Setups')
 
 
 get_downloads()
